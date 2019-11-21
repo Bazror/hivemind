c44403729036c4e308501916a24968d3d6fa4c0c
imwatsi <31913074+imwatsi@users.noreply.github.com>
2019-11-21 14:10:16 +0200

    native ads working code

--- hive/indexer/native_ad.py
+++ hive/indexer/native_ad.py
@@ -19,7 +19,7 @@ class NativeAd:
             # check ad metadata
             ad_metadata = cls._check_ad_metadata(json.loads(entry['json']))
             if ad_metadata is not None:
-                # build ad post
+                # build ad post (mandatory)
                 post = [
                     ('post_id', entry['post_id']),
                     ('community_id', entry['community_id']),
@@ -27,21 +27,29 @@ class NativeAd:
                     ('properties', json.dumps(ad_metadata['properties'])),
                     ('time_units', ad_metadata['time_units'])
                 ]
+                # optional parameters
+                if 'start_time' in ad_metadata:
+                    post.append(
+                        ('start_time', ad_metadata['start_time'])
+                    )
                 return cls._insert(post)
 
         return None
 
     @classmethod
     def _insert(cls, values):
-        return DB.build_insert('hive_posts_cache', values, pk='post_id')
+        return DB.build_insert('hive_ads', values, pk='post_id')
 
     @staticmethod
     def _check_ad_metadata(data):
         if 'native_ad' in data:
-            # validate ad metadata
+            # check ad metadata integrity (mandatory fields)
             ad_metadata = data['native_ad']
-            if 'type' in ad_metadata and 'properties' in ad_metadata:
-                ad_props = ad_metadata['properties']
-                if isinstance(ad_props, dict):  # properties must be in a dict
-                    return ad_metadata
+            fields = ['type', 'properties', 'time_units']
+            for k in fields:
+                if k not in ad_metadata: return None
+            # validate properties dict
+            ad_props = ad_metadata['properties']
+            if isinstance(ad_props, dict):  # properties must be in a dict
+                return ad_metadata
         return None
