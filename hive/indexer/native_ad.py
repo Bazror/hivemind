014db427e334e58afba72782d7e66ac2ffbbec0a
imwatsi <31913074+imwatsi@users.noreply.github.com>
2019-12-04 18:36:11 +0200

    remove obsolete checks

--- hive/indexer/native_ad.py
+++ hive/indexer/native_ad.py
@@ -5,11 +5,10 @@ from hive.db.adapter import Db
 DB = Db.instance()
 
 class NativeAd:
-    """Validate ad posts from cached_post, hook for hive_ads entries"""
+    """Handles the validation of ad posts and returns SQL statements"""
     sql_buffer = []
 
-    @classmethod
-    def process_ad(cls, values):
+    def process_ad(self, values, new=True):
         """Given a cached post insert value set,
         generate SQL statements for valid native ads
         and add to sql_buffer"""
@@ -17,7 +16,7 @@ class NativeAd:
         # check declined status
         if 'is_declined' in entry and entry['is_declined']:
             # check ad metadata
-            ad_metadata = cls._check_ad_metadata(json.loads(entry['json']))
+            ad_metadata = self._check_ad_metadata(json.loads(entry['json']))
             if ad_metadata is not None:
                 # build ad post (mandatory)
                 post = [
@@ -25,12 +24,15 @@ class NativeAd:
                     ('type', ad_metadata['type']),
                     ('properties', json.dumps(ad_metadata['properties']))
                 ]
-                return cls._insert(post)
+                if new:
+                    return self._insert(post)
+                else:
+                    # TODO: self._update(post)
+                    pass
 
         return None
 
-    @classmethod
-    def _insert(cls, values):
+    def _insert(self, values):
         return DB.build_insert('hive_ads', values, pk='post_id')
 
     @staticmethod
@@ -38,16 +40,12 @@ class NativeAd:
         if 'native_ad' in data:
             # check ad metadata integrity (mandatory fields)
             ad_metadata = data['native_ad']
-            fields = ['type', 'properties', 'time_units']
+            fields = ['type', 'properties']
             for k in fields:
                 if k not in ad_metadata: return None
             # validate all internal data
             if len(ad_metadata['type']) <= 16:
                 ad_props = ad_metadata['properties']
-                ad_time = ad_metadata['time_units']
                 if isinstance(ad_props, dict):   # properties must be in a dict
-                    if isinstance(ad_time, int):  # time units must be int
-                        if ad_time < 2147483647:  # respect SQL max int
-                            # TODO [beta]: check start time if present
-                            return ad_metadata
+                    return ad_metadata
         return None
